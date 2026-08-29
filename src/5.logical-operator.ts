67b345af41e1a290d62a9099a14b63a31b63ee29
asm-saim <saim777m@gmail.com>
2026-08-29 22:22:04 +0600

    nullish coaliesing recapped

--- src/5.logical-operator.ts
+++ src/5.logical-operator.ts
@@ -13,8 +13,23 @@ const product = "Laptop";
 const productStat = product && "Available product is " + product;
 console.log(productStat);
 
-//Optional Chaining:
+//Optional Chaining: to prevent error. even the property do not exist.
 const user = null;
-
 const userName = user?.name;
 console.log(userName);
+
+//Nullish Coalescing:(null, undefined)
+const userInfo = null;
+if (userInfo) {
+  console.log(userInfo);
+} else {
+  console.log("Unavailable");
+}
+//ternary:
+userInfo ? console.log(userInfo) : console.log("Unavailable");
+
+//short circuit:
+userInfo && console.log(userInfo);
+
+//nullish:
+console.log(userInfo ?? "Unavailable");
