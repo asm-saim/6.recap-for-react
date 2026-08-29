9a696a97f53ecc7738cfc8a0dd92fbed84b83cc6
asm-saim <saim777m@gmail.com>
2026-08-29 11:34:40 +0600

    map recapped

--- src/3.methods.ts
+++ src/3.methods.ts
@@ -9,3 +9,14 @@
 const num = [3, 4, 5, 6, 7, 8, 76];
 const newNum = num.filter((num) => num > 6);
 console.log(newNum);
+
+//map:
+const values = [3, 55, 7, 88, 9];
+const newValue = values.map((value) => value + 1);
+console.log(newValue);
+
+//Real life Example: (react analogy)
+const names = ["Hakan", "Kamal", "Erbakan"];
+const newnames = names.map((name, index) => {
+  console.log(`${index + 1}. ${name} `);
+});
