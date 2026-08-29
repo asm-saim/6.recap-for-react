9f7243ad5ad7590400ebb3db1b3163c0bb11c8d8
asm-saim <saim777m@gmail.com>
2026-08-29 18:16:45 +0600

    short circuit recapped

---
+++ src/5.logical-operator.ts
@@ -0,0 +1,14 @@
+//logical &&:
+const age = 35;
+const stat = age && "Your age is " + age; //short circuit
+console.log(stat);
+
+//Ex: short circuit example:
+const name = "JK";
+const nameChecker = name && "Name is " + name;
+console.log(nameChecker);
+
+//short circuit example:
+const product = "Laptop";
+const productStat = product && "Available product is " + product;
+console.log(productStat);
