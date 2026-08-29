62dd3d37af1e45efac88cf4294603fbaa19e089f
asm-saim <saim777m@gmail.com>
2026-08-29 10:59:00 +0600

    filter recapped

---
+++ src/3.methods.ts
@@ -0,0 +1,11 @@
+// Array Methods: map(), filter(), find(),
+// Array, String methods:  includes()
+// map() - showing the data in the UI in react.
+// filter() - showing the filtered data.
+// find() - find out a specific data.
+// includes() - a data in array exist ot not. Returns boolean result.
+
+//filter:
+const num = [3, 4, 5, 6, 7, 8, 76];
+const newNum = num.filter((num) => num > 6);
+console.log(newNum);
--- src/checkList.ts
+++ src/checkList.ts
@@ -1,3 +1,5 @@
 /*
 1. Destructuring- object, array
+2. Spread Operator - array, object
+3. Array methods - filter, find, map, includes
 */
