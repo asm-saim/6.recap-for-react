027e74ba05a85db5cfc6d3974fd3a15915566ce5
asm-saim <saim777m@gmail.com>
2026-08-29 17:52:39 +0600

    ternery recapped

---
+++ src/4.ternery-operator.ts
@@ -0,0 +1,10 @@
+//Short form of if else is ternary operator. (ie-to)
+
+const value1 = 3;
+const value2 = 6;
+value1 > value2 ? console.log(`${value1} 1 is bigger`) : console.log(`${value2} is bigger`);
+
+//Ex2:
+const age = 34;
+const ageEligibility = age >= 18 ? "Adult" : "Child";
+console.log(ageEligibility);
--- src/checkList.ts
+++ src/checkList.ts
@@ -1,5 +1,6 @@
 /*
 1. Destructuring- object, array
 2. Spread Operator - array, object
-3. Array methods - filter, find, map, includes
+3. Array methods - map(), filter(), find(), includes(), forEach()
+4. 
 */
